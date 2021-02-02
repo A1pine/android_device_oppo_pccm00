760bfe12150fc5080f700ecc5bd3d7c30a163eff
A1pine <5996013@qq.com>
2021-02-02 23:10:09 +0800

    pccm00:Add Dex props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,16 @@ TARGET_BOOTLOADER_BOARD_NAME := sm8150
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+        endif
+    endif
+endif
+
 # VNDK
 BOARD_VNDK_VERSION := current
 
